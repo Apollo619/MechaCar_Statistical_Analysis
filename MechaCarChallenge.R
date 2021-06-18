d777bf6f2b3b9388615b4389b8123ac0b75d4fe1
Apollo619 <ryan48meredith@yahoo.com>
2021-06-18 11:48:32 -0500

    module 15 challenge stuff

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,23 @@ summary(linear)
 
 ## get r-squared value
 summary(linear)$r.squared
+
+## reading csv file
+Suspension_table <- read.csv(file="Suspension_Coil.csv", check.names=F, stringsAsFactors = F)
+
+## total summary dataframe
+total_summary <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+## lot summary dataframe
+lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+## t.test for PSI across all lots
+t.test(Suspension_table$PSI, mu=mean(1500))
+
+## t.test for PSI across each lot
+lot1 <- Suspension_table %>% filter(Manufacturing_Lot=='Lot1')
+lot2 <- Suspension_table %>% filter(Manufacturing_Lot=='Lot2')
+lot3 <- Suspension_table %>% filter(Manufacturing_Lot=='Lot3')
+t.test(lot1$PSI, mu=mean(Suspension_table$PSI))
+t.test(lot2$PSI, mu=mean(Suspension_table$PSI))
+t.test(lot3$PSI, mu=mean(Suspension_table$PSI))
